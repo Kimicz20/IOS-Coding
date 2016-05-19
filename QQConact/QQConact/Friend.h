1517195916c129056eb934acbe3868456c359136
Kimicz20 <hz2z0421@163.com>
2016-05-19 14:48:59 +0800

    QQ好友列表

---
+++ QQConact/QQConact/Friend.h
@@ -0,0 +1,13 @@
+//
+//  Friend.h
+//  QQConact
+//
+//  Created by Geek on 16/5/19.
+//  Copyright © 2016年 Geek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Friend : NSObject
+
+@end
---
+++ QQConact/QQConact/Friend.m
@@ -0,0 +1,13 @@
+//
+//  Friend.m
+//  QQConact
+//
+//  Created by Geek on 16/5/19.
+//  Copyright © 2016年 Geek. All rights reserved.
+//
+
+#import "Friend.h"
+
+@implementation Friend
+
+@end
